ca77fa99a4684e5e19a85905e8e80fe9032da3e4
Nenu Adrian <adrian.nenu@gmail.com>
2022-08-26 13:28:59 -0400

    Update header.php

--- server/src/views/header.php
+++ server/src/views/header.php
@@ -3,6 +3,7 @@
 <head>
     <title>Administration</title>
     <link rel="stylesheet"  href="./assets/Simple-Grid/simple-grid.min.css">
+    <link rel="stylesheet"  href="./assets/css/styles.css">
 </head>
 <body>
     <div class="container">
