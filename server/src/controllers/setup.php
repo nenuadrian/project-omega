d2f1c5e8aea04c354b3280d8bfa0a7a6bae382c0
Nenu Adrian <adrian.nenu@gmail.com>
2022-09-26 12:23:04 +0100

    Update setup.php

--- server/src/controllers/setup.php
+++ server/src/controllers/setup.php
@@ -3,6 +3,14 @@
 class SetupController extends Controller {
     function index(): void {
         if (Input::post('action') === 'install') {
+            $configs = [
+                "db_host" => Input::post('db_host'),
+                "db_user" => Input::post('db_user'),
+                "db_pass" => Input::post('db_pass'),
+                "db_name" => Input::post('db_name')
+            ];
+            file_put_contents('server/src/configs/environment.json', json_encode($configs));
+            echo 'done';
             die();
         }
         $this->render('setup');
