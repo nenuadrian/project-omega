516a59a68116866acfc9143682dd1323e948263e
Nenu Adrian <adrian.nenu@gmail.com>
2022-09-26 12:23:57 +0100

    Update setup.php

--- server/src/views/setup.php
+++ server/src/views/setup.php
@@ -2,8 +2,12 @@
 <div class="row">
     <div class="col-12">
     <form method="post">
-<button type="submit" class="btn btn-success" name="action" value="install">Configure</button>
-</form>
+        <input type="text" name="db_host" placeholder="db_host"/>
+        <input type="text" name="db_user" placeholder="db_user"/>
+        <input type="text" name="db_pass" placeholder="db_pass"/>
+        <input type="text" name="db_name" placeholder="db_name"/>
+        <button type="submit" class="btn btn-success" name="action" value="install">Configure</button>
+    </form>
 </div>
 </div>
 
