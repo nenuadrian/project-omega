5f28028a9899c73a3a71402e7c40ba66f034d8e4
Nenu Adrian <adrian.nenu@gmail.com>
2022-09-28 12:00:14 +0100

    Update header.php

--- server/src/views/header.php
+++ server/src/views/header.php
@@ -4,6 +4,8 @@
     <title><?=$title?></title>
     <link rel="stylesheet"  href="/assets/Simple-Grid/simple-grid.min.css">
     <link rel="stylesheet"  href="/assets/css/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 </head>
 <body>
     <div class="container">
