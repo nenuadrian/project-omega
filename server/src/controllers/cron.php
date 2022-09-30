70c30e86df97cad84bef01d9d286f24651dc10bf
Nenu Adrian <adrian.nenu@gmail.com>
2022-09-30 10:06:34 +0100

    Update cron.php

--- server/src/controllers/cron.php
+++ server/src/controllers/cron.php
@@ -2,33 +2,16 @@
 
 class CronController extends Controller {
     function index(): void {
-        DB::query('UPDATE stats SET coins = coins + 2');
-        $this->json(200, null);
+        $this->json(200);
     }
 
     function hourly(): void {
-        DB::query('UPDATE stats SET points = coins');
-        $users = DB::query('select * from stats order by points desc');
-        foreach($users as $key => $user) {
-            DB::query('UPDATE stats SET ranking = %d where user_id = %d', $key + 1, $user['user_id']);
-        }
-        unset($users);
-
-        DB::query('UPDATE organization SET points = coins');
-        $organizations = DB::query('select * from organization order by points desc');
-        foreach($organizations as $key => $org) {
-            DB::query('UPDATE organization SET ranking = %d where organization_id = %d', $key + 1, $org['organization_id']);
-        }
-
-        unset($organizations);
-
-        // badges
-        $this->json(200, null);
+       
+        $this->json(200);
     }
 
     function test(): void {
         Admin::generateUsers(10);
-        Admin::generateServers();
-        $this->json(200, null);
+        $this->json(200);
     }
 }
