639a5d20d2dea2374215002175ca7c5b88a1acac
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-05-28 19:32:30 +0100

    Update view.php

--- server/src/core/view.php
+++ server/src/core/view.php
@@ -3,6 +3,12 @@
 abstract class View {
   public static function render(string $view, array $variables = []): void {
         extract($variables);
-        require SRC_DIR . "/views/$view.php"; 
+        $viewFile = SRC_DIR . "/views/$view.php";
+        
+        if (file_exists($viewFile)) {
+            require $viewFile;
+        } else {
+            throw new Exception("View file not found: $view");
+        }
     }
 }
