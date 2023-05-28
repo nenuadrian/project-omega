1210c005657893a2c6c22c2fdec9db85b549faf7
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-05-28 19:31:40 +0100

    Update routes.php

--- server/src/core/routes.php
+++ server/src/core/routes.php
@@ -3,7 +3,10 @@
 function routes(?string $page): Controller {
     $className = ucfirst($page) . "Controller";
     $className = class_exists($className) ? $className : 'HomeController';
+
     if (class_exists($className)) {
-        return new $className;
-    } else die('404 - controller not found');
+        return new $className();
+    } else {
+        throw new Exception('404 - Controller not found');
+    }
 }
