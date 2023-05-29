a170d19faa45af47697d1c746701affff3ba6ffa
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-05-29 22:23:11 +0100

    Update home.php

--- server/src/views/home.php
+++ server/src/views/home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html><html lang="en"><head>
   <meta charset="utf-8">
   <title>Client</title>
-  <base href="/project-omega/server/public_html/">
+  <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <style>@import url(https://fonts.googleapis.com/css?family=Lato:400,300,300italic,400italic,700,700italic);body,html{height:100%;width:100%;margin:0;padding:0;left:0;top:0;font-size:100%}.container{margin-left:auto;margin-right:auto}*{font-family:Lato,Helvetica,sans-serif;color:#333447;line-height:1.5}.container{width:90%}@media only screen and (min-width:33.75em){.container{width:80%}}@media only screen and (min-width:60em){.container{width:75%;max-width:60rem}}</style><link rel="stylesheet" href="./assets/Simple-Grid/simple-grid.min.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="./assets/Simple-Grid/simple-grid.min.css"></noscript>
