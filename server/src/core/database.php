dc544d6c68ea2533f2fd5ebb157251d7139c018b
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-10 16:17:34 +0100

    Update database.php

--- server/src/core/database.php
+++ server/src/core/database.php
@@ -8,7 +8,7 @@ if (!file_exists($file)) {
 
 $configs = json_decode(file_get_contents($file), true);
 
-if (isset($configs['db_host'])) {
+if (isset($configs['db_host']) && !empty($configs['db_host'])) {
     DB::$host = $configs['db_host'];
     DB::$user = $configs['db_user'];
     DB::$password = $configs['db_pass'];
