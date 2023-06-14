19648b7719caf3a2c8bd5791e43e457513e996e1
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-14 12:27:58 +0100

    Update database.php

--- server/src/core/database.php
+++ server/src/core/database.php
@@ -1,16 +1,8 @@
 <?php declare (strict_types = 1);
 
-$file = dirname(__FILE__) . '/../configs/environment.json';
-
-if (!file_exists($file)) {
-    die('Missing environment configs');
-}
-
-$configs = json_decode(file_get_contents($file), true);
-
-if (isset($configs['db_host']) && !empty($configs['db_host'])) {
-    DB::$host = $configs['db_host'];
-    DB::$user = $configs['db_user'];
-    DB::$password = $configs['db_pass'];
-    DB::$dbName = $configs['db_name'];
+if (Configs::get('db_host')) {
+    DB::$host = Configs::get('db_host');
+    DB::$user = Configs::get('db_user');
+    DB::$password = Configs::get('db_pass');
+    DB::$dbName = Configs::get('db_name');
 }
