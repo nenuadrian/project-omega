da485bd7c502f79c015c78de06559235480856c4
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-15 21:48:27 +0100

    Captcha

--- server/src/controllers/auth.php
+++ server/src/controllers/auth.php
@@ -57,7 +57,22 @@ class AuthController extends Controller {
               "sessionHash" => $session_hash
           ]);
     }
-  
+
+    private function captcha($token, $action) {
+        // call curl to POST request 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => Configs::get('captcha'), 'response' => $token)));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $arrResponse = json_decode($response, true);
+         
+        // verify the response 
+        return $arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5;
+    }
+        
     function register(): void {
           if (Input::post('action') == 'register') {
             $session_hash = $this->doRegister();
