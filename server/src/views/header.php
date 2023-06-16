9632b931fa582d0c3c2dc7c2b755034a39a419a0
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-16 14:21:13 +0100

    Update header.php

--- server/src/views/header.php
+++ server/src/views/header.php
@@ -3,9 +3,8 @@
 <head>
     <title><?=$title?></title>
     <base href="/project-omega/server/public_html/">
-    <link rel="stylesheet"  href="./assets/Simple-Grid/simple-grid.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
+    <link rel="stylesheet"  href="https://bootswatch.com/5/flatly/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
