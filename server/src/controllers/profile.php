2f20e6a31391a48cabd866a03dd242f5d6c43699
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-16 12:11:16 +0100

    Update profile.php

--- server/src/controllers/profile.php
+++ server/src/controllers/profile.php
@@ -7,14 +7,10 @@ class ProfileController extends GuardController {
     
     function view_rest(string $userId): void {
         $user   = Stats::byId(intval($userId));
-        $badges = Badge::byUserId(intval($userId));
-        $orgs   = Organizations::byUserId(intval($userId));
 
         $this->json(200, [
             'user'          => $user,
             'rank'          => $user,
-            'badges'        => $badges,
-            'organizations' => $orgs
         ]);
     }
 
