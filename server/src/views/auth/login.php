1438ad35fc49b1209da9441c78d9b2e0910d607f
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-16 14:19:40 +0100

    Update login.php

--- server/src/views/auth/login.php
+++ server/src/views/auth/login.php
@@ -1,11 +1,18 @@
 <?php View::render('header', ['title' => 'Login']); ?>
 
-<form method="post">
-  <input type="text" name="id" placeholder="Username or E-mail"/>
-  <input type="password" name="password" placeholder="Password"/>
-  <button type="submit" name="action" value="login">
-    Login
-  </button>
-</form>
+<div class="row">
+  <div class="col-md-3">
+    
+  </div>
+  <div class="col-md-6 text-center">
+    <form method="post">
+      <input class="form-control" type="text" name="id" placeholder="Username or E-mail"/>
+      <input class="form-control" type="password" name="password" placeholder="Password"/>
+      <button class="btn btn-primary" type="submit" name="action" value="login">
+        Login
+      </button>
+    </form>
+  </div>
+</div>
 
 <?php View::render('footer'); ?>
