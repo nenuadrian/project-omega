44139c423b79e3cc0cc4cb09ddf0c9f92d0176c0
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-17 13:03:49 +0100

    Update login.php

--- server/src/views/auth/login.php
+++ server/src/views/auth/login.php
@@ -4,16 +4,25 @@
   <div class="col-md-3">
     
   </div>
-  <div class="col-md-6 text-center">
-    <form method="post">
-      <input class="form-control" type="text" name="id" placeholder="Username or E-mail"/>
-      <input class="form-control" type="password" name="password" placeholder="Password"/>
-      <button class="btn btn-primary" type="submit" name="action" value="login">
-        Login
-      </button>
-    </form>
-    <br/>
-    <a class="btn btn-sm btn-primary" href="<?=BASE_URL?>/auth/register">Create a new account</a>
+  <div class="col-md-6">
+    <?php if (isset($errors) && !empty($errors)): ?>
+        <?php foreach ($errors as $error): ?>
+        <div class="alert alert-danger">
+            <?=$error?>
+        </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    <div class=" text-center">
+        <form method="post">
+        <input class="form-control" type="text" name="id" placeholder="Username or E-mail"/>
+        <input class="form-control" type="password" name="password" placeholder="Password"/>
+        <button class="btn btn-primary" type="submit" name="action" value="login">
+            Login
+        </button>
+        </form>
+        <br/>
+        <a class="btn btn-sm btn-primary" href="<?=BASE_URL?>/auth/register">Create a new account</a>
+    </div>
   </div>
 </div>
 
