e43e44de20f23bd1606c91dfe8cded3fd1817eb0
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-17 10:59:03 +0100

    Update header.php

--- server/src/views/header.php
+++ server/src/views/header.php
@@ -3,7 +3,9 @@
 <head>
     <title><?=$title?></title>
     <base href="/project-omega/server/public_html/">
-    <link rel="stylesheet"  href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+   
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
