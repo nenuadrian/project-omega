769db8cd57272b58daf03674ca85bd284b011b3b
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-17 10:40:04 +0100

    Update header.php

--- server/src/views/header.php
+++ server/src/views/header.php
@@ -3,11 +3,28 @@
 <head>
     <title><?=$title?></title>
     <base href="/project-omega/server/public_html/">
-    <link rel="stylesheet"  href="https://bootswatch.com/5/flatly/bootstrap.min.css">
+    <link rel="stylesheet"  href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
+    <br/>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor02">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="<?=BASE_URL?>">Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <br/>
     <br/>
 
