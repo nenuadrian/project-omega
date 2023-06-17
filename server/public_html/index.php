b3921e7a07d1151b50ed3911ad0ed51764c2b318
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-17 12:35:57 +0100

    Update index.php

--- server/public_html/index.php
+++ server/public_html/index.php
@@ -22,6 +22,35 @@ require CORE_DIR . '/src/core/core.php';
 
 Profiler::start();
 
+if(Input::cookie('consent') === null) {
+	// First time visitor
+	// Get the user countrycode by ip
+	if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+		$ip = $_SERVER['REMOTE_ADDR'];
+	}
+	$geo = json_decode(file_get_contents('http://www.geoplugin.net/json.gp?ip='.$ip),True);
+	$country_codesEU=['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'GB', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK'];
+  if ($geo['geoplugin_countryCode'] == Null || in_array($geo['geoplugin_countryCode'],$country_codesEU)){
+		// User is in the EU or we do not know where he is from.
+		// Ask for cookies consent, if within 15 minutes the user comes back, they accept
+		setcookie("consent", 'false', time() + (60*15));
+	}else{
+		// The user is not in the EU, so we can set cookies
+		setcookie("consent", 'true', time() + (60*15));
+	}
+} else if (Input::cookie('consent') == 'true') {
+    session_start();
+} 
+
+if (Input::cookie('consent') === 'false') {
+  if (Input::post('cookies') === 'accept') {
+    $_COOKIE['consent'] = 'true';
+		setcookie("consent", 'true', time() + (90 * 24 * 60 * 60));
+  }
+}
+
 mvc();
 
 Profiler::end();
