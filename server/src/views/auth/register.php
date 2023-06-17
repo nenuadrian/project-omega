8f8585bdb9efb5a996292897763c7885d6c190b3
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-17 12:08:41 +0100

    Update register.php

--- server/src/views/auth/register.php
+++ server/src/views/auth/register.php
@@ -5,9 +5,16 @@
   </div>
   <div class="col-md-6 text-center">
       <form method="post">
-        <input class="form-control" required type="email" name="email" placeholder="E-mail"/>
-        <input class="form-control" required type="text" name="username" placeholder="Username"/>
-        <input class="form-control" required type="password" name="password" placeholder="Password"/>
+        <?php if (isset($errors) && !empty($errors)): ?>
+          <?php foreach ($errors as $error): ?>
+            <div class="alert alert-danger">
+              <?=$error?>
+            </div>
+          <?php endforeach; ?>
+        <?php endif; ?>
+        <input class="form-control" required type="email" name="email" placeholder="E-mail" value="<?=Input::post('email')?>"/>
+        <input class="form-control" required type="text" name="username" placeholder="Username" value="<?=Input::post('username')?>"/>
+        <input class="form-control" required type="password" name="password" placeholder="Password" value="<?=Input::post('password')?>"/>
         <button class="btn btn-lg btn-primary" type="submit" name="action" value="register">
           Create account
         </button>
