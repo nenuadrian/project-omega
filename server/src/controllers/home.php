c54dd2169d5163bd1d4013f38630ed1bc373807f
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-18 16:07:58 +0100

    Update home.php

--- server/src/controllers/home.php
+++ server/src/controllers/home.php
@@ -4,4 +4,18 @@ class HomeController extends Controller {
     function index(): void {
         View::render('home');
     }
+
+    function subscribe(): void {
+        \Stripe\Stripe::setApiKey(Configs::get('stripe'));
+        $lineItems = [];
+        $lineItems[] = ['price' => '', 'quantity' => 1 ];
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'line_items' => $lineItems,
+            'mode' => 'subscription',
+            'success_url' => BASE_URL . '/home/thanks',
+            'cancel_url' => BASE_URL,
+          ]);
+
+        $this->redirect($checkout_session->url);
+    }
 }
