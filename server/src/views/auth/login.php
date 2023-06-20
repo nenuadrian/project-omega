2c6d83a30b8a2fe00fc80bd2b543bc37c19784e7
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2023-06-20 11:06:26 +0100

    Update login.php

--- server/src/views/auth/login.php
+++ server/src/views/auth/login.php
@@ -17,6 +17,14 @@
                 <input class="form-control" type="text" autofocus autocorrect="off" autocapitalize="none" name="id"
                     placeholder="Username or E-mail" required value="<?=Input::post('id')?>" />
                 <input class="form-control" type="password" name="password" placeholder="Password" required />
+                <?php if (Configs::get('captcha')): ?>
+                    <br/>
+                    <br/>
+                      <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+                
+                    <div class="g-recaptcha" style="display:inline-block" data-sitekey="6Le-pZ8mAAAAAEC1w-UEh2QX-p7lvq5IBPJtiD8E"></div>
+                                <br/>
+                    <?php endif; ?>
                 <button class="btn btn-primary" type="submit" name="action" value="login">
                     <i class="fa-solid fa-face-smile"></i> Login
                 </button>
