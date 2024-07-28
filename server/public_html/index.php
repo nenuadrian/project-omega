073d1aaa0b9dd3ea14d8aebc09b07099d5d60770
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2024-07-28 19:43:53 +0100

    Update index.php - session check

--- server/public_html/index.php
+++ server/public_html/index.php
@@ -34,23 +34,25 @@ if(Input::cookie('consent') === null) {
 	}
 	$geo = json_decode(file_get_contents('http://www.geoplugin.net/json.gp?ip='.$ip),True);
 	$country_codesEU=['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'GB', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK'];
-  if ($geo['geoplugin_countryCode'] == Null || in_array($geo['geoplugin_countryCode'],$country_codesEU)){
+    if ($geo['geoplugin_countryCode'] == Null || in_array($geo['geoplugin_countryCode'],$country_codesEU)){
 		// User is in the EU or we do not know where he is from.
 		// Ask for cookies consent, if within 15 minutes the user comes back, they accept
 		setcookie("consent", 'false', time() + (60*15), "/");
-	}else{
+	} else {
 		// The user is not in the EU, so we can set cookies
 		setcookie("consent", 'true', time() + (60*15), "/");
 	}
 } else if (Input::cookie('consent') == 'true') {
-    session_start();
-} 
+	if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}
+}
 
 if (Input::cookie('consent') === 'false') {
-  if (Input::post('cookies') === 'accept') {
-    $_COOKIE['consent'] = 'true';
-		setcookie("consent", 'true', time() + (90 * 24 * 60 * 60), "/");
-  }
+    if (Input::post('cookies') === 'accept') {
+		$_COOKIE['consent'] = 'true';
+			setcookie("consent", 'true', time() + (90 * 24 * 60 * 60), "/");
+	}
 }
 
 mvc();
