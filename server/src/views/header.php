e213518b4c330cfa297d636d1efe3d6a93675164
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2024-07-28 20:00:39 +0100

    Update header.php - charts

--- server/src/views/header.php
+++ server/src/views/header.php
@@ -12,6 +12,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
         crossorigin="anonymous" referrerpolicy="no-referrer">
+    <script src="
+        https://cdn.jsdelivr.net/npm/vis@4.21.0-EOL/dist/vis.min.js
+        "></script>
+        <link href="
+        https://cdn.jsdelivr.net/npm/vis@4.21.0-EOL/dist/vis.min.css
+        " rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
