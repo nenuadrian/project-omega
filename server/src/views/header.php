db3a879a280c810afcc390fa9856b81637875305
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2024-08-10 14:37:37 +0100

    Update header.php

--- server/src/views/header.php
+++ server/src/views/header.php
@@ -4,10 +4,18 @@
 <head>
     <meta charset="utf-8">
     <title><?=isset($title) ? $title : 'APP'?></title>
-    <link rel="stylesheet" href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+        rel="stylesheet">
+    
+    <link rel="stylesheet" href="/styles.css">
     <link rel="icon" type="image/png" href="/assets/img/icon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
