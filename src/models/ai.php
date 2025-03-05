924fa6901c3dc63a1987d89506c89f2c2e9e3f5e
Adrian (Mircea) Nenu <adrian.nenu@gmail.com>
2025-03-04 20:05:26 -0500

    Update ai.php

--- src/models/ai.php
+++ src/models/ai.php
@@ -5,7 +5,7 @@ class AI
     public static $endpoint = 'https://api.openai.com/v1/chat/completions';
     public static $max_tokens = 700;
 
-    public static function chat(array $messages)
+    public static function chat(array $messages, string $model = 'gpt-4o-mini')
     {
         $headers = [
             'Content-Type' => 'application/json',
@@ -13,7 +13,7 @@ class AI
         ];
 
         $data = [
-            'model' => 'gpt-4o-mini',
+            'model' => $model,
             'messages' => $messages,
             "max_tokens" => AI::$max_tokens, // Adjust as needed
             "temperature" => 0.7, // Adjust as needed
@@ -25,6 +25,11 @@ class AI
         $response = WpOrg\Requests\Requests::post(AI::$endpoint, $headers, json_encode($data), $options);
 
         $decodedResponse = json_decode($response->body, true);
+
+        if (isset($decodedResponse['error'])) {
+            return $decodedResponse['error']['message'];
+        }
+
         return $decodedResponse['choices'][0]['message']['content'];
     }
 }
